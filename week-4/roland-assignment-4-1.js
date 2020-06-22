e13a75eaf2ce6e3c29a6633dfbe57fb3595e203c
Devan Wong <devan.wong@yahoo.com>
2020-06-21 21:24:58 -0700

    corrected: display your array into a string

--- week-4/roland-assignment-4-1.js
+++ week-4/roland-assignment-4-1.js
@@ -1,16 +1,20 @@
-/*
+   /*
 ============================================
 ; Title:  roland-assignment-4-1.js
-; Author: Jonathan Roland 
+; Author: Jonathan Roland
 ; Date:   18 June 2020
-; Modified By: 
+; Modified By: Devan Wong
 ; Description: A program utilizing an array with at least two errors.
 ;===========================================
 */
 
-var testArray === ("this","is","a","test","array");
+//changed the === to 1 =
+//changed the "()" to "[]"
+var testArray = ["this","is","a","test","array"];
 var output = "";
-for(i=1; i<testArray.length; i+){
-    output+=testArray[i]+" ";
+//created a let variable and changed it to 0
+for(let i = 0; i < testArray.length; i++){
+    output += testArray[i] + " ";
 }
+//pout "this is a test array"
 console.log(output);
